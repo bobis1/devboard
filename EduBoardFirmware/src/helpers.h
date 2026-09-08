c0fb2dd26dc6be1fee23229bb6e6fc84e103c12c
Joseph <josephwaizy@gmail.com>
2026-09-08 18:30:54 -0500

    mobile work

--- EduBoardFirmware/src/helpers.h
+++ EduBoardFirmware/src/helpers.h
@@ -7,7 +7,13 @@
 
 
 
-void editPixel(Vram: std::bitset,x: int, y: int, value: int){
+void editPixel(std::bitset<4096>& vram,x: int, y: int, value: int){
  int i =	y * SCREEN_WIDTH) + x;
-	Vram[i] = value;
+	vram[i] = value;
+}
+
+void drawPicture(std::bitset<4096>& vram,x: int, y: int, std::bitset<8>& picture){
+	///I am not going to lie I have no idea. I think the plan is to make it so
+int i =	y * SCREEN_WIDTH) + x;
+
 }
